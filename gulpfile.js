34b8d9e0fe56d437a95000c9bfdd1b78e9aadfc6
Ostin63 <Ostin63@users.noreply.github.com>
2018-10-15 11:30:02 +0300

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,13 @@ const cleanCSS = require('gulp-clean-css');
 const minify = require('gulp-minify');
 const autoprefixer = require('gulp-autoprefixer');
 const less = require('gulp-less');
-const rename = require('gulp-rename');
+const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
-const w3cValidation = require('gulp-w3c-html-validation');
-
-gulp.task('w3c', function () {
-  return gulp.src('public/**/*.html')
-    .pipe(w3cValidation({
-      //generateCheckstyleReport: 'w3cErrors/validation.xml'
-    }))
-});
-/* 
+ 
 gulp.task('less', function () {
   gulp.src('./assets/less/*.less')
-    .pipe(less('./assets/css/'))
-    .pipe(rename('main.css'))
+    .pipe(less())
+    .pipe(rename("main.css"))
     .pipe(gulp.dest('./assets/css/'))
     .pipe(autoprefixer({
       browsers: ['last 10 versions'],
@@ -28,46 +20,36 @@ gulp.task('less', function () {
     }))
     .pipe(gulp.dest('./public/css/'))
     .pipe(connect.reload());
-});*/
+});
 
 gulp.task('html', function () {
-  return gulp.src('./assets/*.html')
+  gulp.src('./assets/*.html')
     .pipe(gulp.dest('./public/'))
     .pipe(connect.reload());
 });
 
 gulp.task('css', function () {
-  return gulp.src('./assets/css/main.css')
+  gulp.src('./assets/css/*.css')
     .pipe(cleanCSS())
-    .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./public/css/'))
     .pipe(connect.reload());
 });
-
-gulp.task('normalize', function () {
-  return gulp.src('./assets/css/normalize.css')
-    .pipe(cleanCSS())
-    .pipe(rename('normalize.min.css'))
-    .pipe(gulp.dest('./public/css/'))
-    .pipe(connect.reload());
-});
-
 
 gulp.task('js', function () {
-  return gulp.src('./assets/js/*.js')
+  gulp.src('./assets/js/*.js')
     .pipe(minify())
     .pipe(gulp.dest('./public/js/'))
     .pipe(connect.reload());
 });
 
 gulp.task('fonts', function () {
-  return gulp.src('./assets/fonts/*')
+  gulp.src('./assets/fonts/*')
     .pipe(gulp.dest('./public/fonts/'))
     .pipe(connect.reload());
 });
 
 gulp.task('img', function () {
-  return gulp.src('./assets/img/**/*')
+  gulp.src('./assets/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./public/img/'))
     .pipe(connect.reload());
@@ -94,5 +76,5 @@ gulp.task('watch', function () {
   gulp.watch("./assets/less/*.less", ["less"]);
 });
 
-gulp.task('build', (callback) => runSequence('clean', ['html', 'css', 'normalize', 'less', 'js', 'img', 'fonts'], 'w3c' , callback));
+gulp.task('build', (callback) => runSequence('clean', ['html', 'css', 'less', 'js', 'img', 'fonts'], callback));
 gulp.task('default', (callback) => runSequence('build', 'connect', 'watch', callback));
